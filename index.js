9a719a9ddd67ed23f7e877887eaa66b63b173a2a
Nacho Bonafonte <nacho.bonafontearruga@datadoghq.com>
2023-01-04 10:11:13 +0100

    Added more feedback from code review

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ function getFrameworkPathForPlatform(platform) {
 async function deleteLinesContaining(file, match) {
   let newName = file + "_old";
   await io.mv(file, newName);
-  fs.readFile(newName, { encoding: "utf-8" }, function(err, data) {
+  fs.readFileSync(newName, { encoding: "utf-8" }, function(err, data) {
     if (err) throw error;
 
     let dataArray = data.split("\n"); // convert file data in an array
@@ -244,7 +244,7 @@ async function deleteLinesContaining(file, match) {
     // IN CASE YOU WANT TO UPDATE THE CONTENT IN YOUR FILE
     // THIS WILL REMOVE THE LINE CONTAINS 'user1' IN YOUR shuffle.txt FILE
     const updatedData = dataArray.join("\n");
-    fs.writeFile(file, updatedData, err => {
+    fs.writeFileSync(file, updatedData, err => {
       if (err) throw err;
       console.log("Successfully updated the file data");
     });
@@ -500,37 +500,6 @@ function recFindByExt(base, ext, files, result) {
     }
   });
   return result;
-}
-
-async function deleteLinesContaining(file, match) {
-  let newName = file + "_old";
-  await io.mv(file, newName);
-  fs.readFile(newName, { encoding: "utf-8" }, function(err, data) {
-    if (err) throw error;
-
-    let dataArray = data.split("\n"); // convert file data in an array
-    const searchKeyword = match; // we are looking for a line, contains, key word 'user1' in the file
-    let lastIndex = -1; // let say, we have not found the keyword
-
-    for (let index = 0; index < dataArray.length; index++) {
-      if (dataArray[index].includes(searchKeyword)) {
-        // check if a line contains the 'user1' keyword
-        lastIndex = index; // found a line includes a 'user1' keyword
-        break;
-      }
-    }
-
-    dataArray.splice(lastIndex, 1); // remove the keyword 'user1' from the data Array
-
-    // UPDATE FILE WITH NEW DATA
-    // IN CASE YOU WANT TO UPDATE THE CONTENT IN YOUR FILE
-    // THIS WILL REMOVE THE LINE CONTAINS 'user1' IN YOUR shuffle.txt FILE
-    const updatedData = dataArray.join("\n");
-    fs.writeFile(file, updatedData, err => {
-      if (err) throw err;
-      console.log("Successfully updated the file data");
-    });
-  });
 }
 
 async function swiftPackageRun(platform, extraParameters, itrEnabled) {
